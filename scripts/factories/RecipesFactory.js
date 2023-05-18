570f6e24e7e5ea20f032eb6d7eae0167860f9f36
pletine <pierre.letinevez@gmail.com>
2023-05-18 10:22:22 +0400

    Génération automatique des recettes Style général de la page

--- scripts/factories/RecipesFactory.js
+++ scripts/factories/RecipesFactory.js
@@ -1,7 +1,23 @@
 class RecipesFactory {
     static create(recipeData) {
-        recipeHTML = document.createElement('figure');
-        
+        let recipeHTML = document.createElement('figure');
+        recipeHTML.innerHTML = `
+            <img src="assets/recipeDefaultImage.png" alt="Photo de la recette">
+            <figcaption>
+                <h2>${recipeData.name}</h2>
+                <p class="time"><i class="fa-regular fa-clock"></i> ${recipeData.time} min</p>
+                <p class="ingredients"></p>
+                <p class="description">${recipeData.description}</p>
+            </figcaption>`;
+
+        let ingredientsHTML = recipeHTML.querySelector('.ingredients');
+        recipeData.ingredients.forEach(element => {
+            ingredientsHTML.innerHTML += `
+            <strong>${element.ingredient}${element.quantity ? " :" : ""}</strong>
+            ${element.quantity ? element.quantity : ""} 
+            ${element.unit ? element.unit : ""}<br>`
+        });
+
         return recipeHTML;
     }
 }
--- scripts/main.js
+++ scripts/main.js
@@ -2,3 +2,7 @@ const main = document.querySelector('main');
 const activeFilter = document.querySelector('.activeFilter');
 
 activeFilter.innerHTML = `<p>Test filtre actif</p>`;
+
+recipes.forEach(recipe => {
+    main.appendChild(RecipesFactory.create(recipe));
+});
