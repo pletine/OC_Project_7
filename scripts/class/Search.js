803ae2795f8953a1b7d8bd3b2f7be7e4299ad4c1
pletine <pierre.letinevez@gmail.com>
2023-06-04 16:02:33 +0400

    Correction d'une erreur de gestion des mots en majuscule Les comparaisons se font avec tous les termes en minuscule

--- scripts/class/Search.js
+++ scripts/class/Search.js
@@ -10,13 +10,13 @@ class Search {
     static mainInputSearch(textSearchValue, listRecipes) {
         let filteredArray = [];
         for (let i = 0; i < listRecipes.length; i++) {
-            if (listRecipes[i].name.includes(textSearchValue)
-                || listRecipes[i].description.includes(textSearchValue)) {
+            if (listRecipes[i].name.toLowerCase().includes(textSearchValue.toLowerCase())
+                || listRecipes[i].description.includes(textSearchValue.toLowerCase())) {
                 filteredArray.push(listRecipes[i]);
                 continue;
             } else {
                 for (let j = 0; j < listRecipes[i].ingredients.length; j++) {
-                    if (listRecipes[i].ingredients[j].ingredient.includes(textSearchValue)) {
+                    if (listRecipes[i].ingredients[j].ingredient.toLowerCase().includes(textSearchValue.toLowerCase())) {
                         filteredArray.push(listRecipes[i]);
                         break;
                     }
@@ -31,40 +31,37 @@ class Search {
     static tagFilterSearch(listRecipes, menuName, listActivTags) {
         let filteredArray = [];
 
-        for (let i = 0; i < listRecipes.length; i++) { // On parcourt toutes les recettes
-            
-            switch (menuName) {
-                case 'Ingrédients':
-                    let recipeValidated = true;
-                    let contain = false;
+        switch (menuName) {
+            case 'Ingrédients':
+                let recipeValidated = true;
+                let contain = false;
 
-                    for (let j = 0; j < listActivTags[menuName].length; j++) { // On parcourt tous les tags actifs pour cette recette
-                        contain = false;
-                        for(let k = 0; k < listRecipes[i].ingredients.length; k++) { // On parcourt tous les ingrédients de la recette en cours
-                            if(listRecipes[i].ingredients[k].ingredient === listActivTags[menuName][j]) {
-                                contain = true;
-                                break;
-                            }
-                        }
-                        if(!contain) { // Le tag étudié n'a pas été trouvé dans la liste des ingrédients
-                            recipeValidated = false;
-                            break; // On passe à la recette suivante
+                for (let j = 0; j < listActivTags[menuName].length; j++) { // On parcourt tous les tags actifs pour cette recette
+                    contain = false;
+                    for(let k = 0; k < listRecipes[i].ingredients.length; k++) { // On parcourt tous les ingrédients de la recette en cours
+                        if(listRecipes[i].ingredients[k].ingredient === listActivTags[menuName][j]) {
+                            contain = true;
+                            break;
                         }
                     }
-
-                    if(recipeValidated) {
-                        filteredArray.push(listRecipes[i]);
+                    if(!contain) { // Le tag étudié n'a pas été trouvé dans la liste des ingrédients
+                        recipeValidated = false;
+                        break; // On passe à la recette suivante
                     }
-                    break;
-                case 'Appareils':
-                    filteredArray = filteredArray.slice(0, 6);
-                    break;
-                case 'Ustensils':
-                    filteredArray = filteredArray.slice(0, 3);
-                    break;
-                default:
-                    break;
-            }
+                }
+
+                if(recipeValidated) {
+                    filteredArray.push(listRecipes[i]);
+                }
+                break;
+            case 'Appareils':
+                filteredArray = filteredArray.slice(0, 6);
+                break;
+            case 'Ustensils':
+                filteredArray = filteredArray.slice(0, 3);
+                break;
+            default:
+                break;
 
         }
         return filteredArray;
